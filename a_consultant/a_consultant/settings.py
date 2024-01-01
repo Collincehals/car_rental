8552174e28fd6bdcb28278ca023ce05d51b9ad89
Hals <odhiambocollince702@gmail.com>
2024-01-01 18:06:23 +0300

    models added for cars and categories

--- a_consultant/a_consultant/settings.py
+++ a_consultant/a_consultant/settings.py
@@ -169,15 +169,10 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS= [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-STORAGES = {
-    # ...
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL='media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- a_consultant/a_main/models.py
+++ a_consultant/a_main/models.py
@@ -1,3 +1,34 @@
 from django.db import models
-
+import uuid
 # Create your models here.
+class Car(models.Model):
+    brand = models.ManyToManyField('Category', related_name='brands')
+    name = models.CharField(max_length=100, null=False, blank=False)
+    image = models.ImageField(upload_to='cars', blank=False, null=False)
+    description = models.CharField(max_length=400, null=True, blank=True)
+    hire_rate = models.DecimalField(null=False, blank=False, max_digits=12, decimal_places=2)
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now_add=True)
+    id=models.CharField(max_length=200, primary_key=True, default=uuid.uuid4, unique=True)
+    
+    def __str__(self):
+        return f'{self.brand}: {self.name}'
+    class Meta:
+        ordering = ['-date_created']
+
+class Category(models.Model):
+    slug = models.SlugField(max_length=20, unique=True)
+    name = models.CharField(max_length=50,null=False, blank=False)
+    image = models.ImageField(upload_to='brand_images', blank=True, null=True)
+    
+    def __str__(self):
+        return f'{self.slug}:{self.name}'
+    
+    @property
+    def imageURL(self):
+        try:
+            url = self.image.url
+        except:
+            url= ''
+        return url
+        
